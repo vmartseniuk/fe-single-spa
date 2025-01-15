79a658501b641d005cdf84fd98850f2f5af6a1cd
Volodymyr Martseniuk <Volodymyr.Martseniuk@firstgroup.com>
2025-01-15 12:14:10 +0200

    added sup routing for child 1

--- child1/src/App.tsx
+++ child1/src/App.tsx
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router'
 
 function App() {
   return (
     <>
       <h4>VITE APP - Child #1</h4>
       <button onClick={() => alert('Child #1 clicked')}>Click me1</button>
+
+      <Router basename='/mifea'>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/page1">page 1</Link></li>
+            <li><Link to="/page2">page 2</Link></li>
+          </ul>
+     
+          <Routes >
+            <Route path="/" element={<div>Home content</div>} />
+            <Route path="/page1" element={<div >Page1 content</div>} />
+            <Route path="/page2" element={<div >Page2 content</div>} />
+
+
+         
+          </Routes>
+        </Router>
     </>
   )
 }
